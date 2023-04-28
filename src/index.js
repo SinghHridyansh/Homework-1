d1c54181e8f1607295736709ddeae64a8f26f060
SinghHridyansh <Hridyansh.360@gmail.com>
2023-04-28 21:11:10 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "../src/index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const element = (
+  <div>
+    <h1 className="my-class">Topics Covered</h1>
+    <p className="my-class">
+      The following is a list of all the topics we cover in the MDN learning
+      area.
+    </p>
+    <a href="..">Getting started with the web</a>
+    <p>
+      Provides a practical introduction to web development for complete
+      beginners.
+    </p>
+    <a href="..">HTML — Structuring the web</a>
+    <p>
+      HTML is the language that we use to structure the different parts of our
+      content and define what their meaning or purpose is. This topic teaches
+      HTML in detail.
+    </p>
+    <a href="..">CSS — Styling the web</a>
+    <p>
+      CSS is the language that we use to control our web content's style and
+      layout, as well as adding behavior like animation. This topic provides
+      comprehensive coverage of CSS.
+    </p>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(element, document.getElementById("root"));
